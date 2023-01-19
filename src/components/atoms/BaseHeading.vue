b23b9146b41c1fcfc0a3ad0f97a28368d71f3645
andota05 <53824832+andota05@users.noreply.github.com>
2023-01-20 06:13:17 +0900

    コンポーネントの整理

---
+++ src/components/atoms/BaseHeading.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="tagName" :class="className">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeadeing',
+  props: {
+    tag: {
+      type: String,
+      required: true
+    },
+    className: String
+  },
+  computed: {
+    tagName() {
+      return this.tag
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
